bbef0515c326188ca564e3045ff0494139d03be2
Devcon324 <devknight324@gmail.com>
2025-11-19 00:50:20 -0500

    rag with neo4j

---
+++ backend/app/services/RAG.py
@@ -0,0 +1,162 @@
+import os
+from pathlib import Path
+from textwrap import dedent
+
+from dotenv import load_dotenv
+from haystack import Document, Pipeline
+from haystack.components.builders import PromptBuilder
+from haystack.components.converters import MarkdownToDocument
+from haystack.components.joiners import DocumentJoiner
+from haystack.components.preprocessors import DocumentCleaner, DocumentSplitter
+from haystack.components.writers import DocumentWriter
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack.document_stores.types import DuplicatePolicy
+from haystack_integrations.components.embedders.ollama import (
+    OllamaDocumentEmbedder, OllamaTextEmbedder)
+from haystack_integrations.components.generators.ollama import OllamaGenerator
+from neo4j_haystack import Neo4jDocumentStore, Neo4jEmbeddingRetriever
+
+load_dotenv(dotenv_path=Path(__file__).parent.parent.parent / ".env")
+
+class RAGService:
+  def __init__(self):
+    self.embedding_pipeline = Pipeline()
+    self.document_store = Neo4jDocumentStore(
+      url=os.getenv("NEO4J_URI"),
+      database=os.getenv("NEO4J_DATABASE"),
+      username=os.getenv("NEO4J_USERNAME"),
+      password=os.getenv("NEO4J_PASSWORD"),
+      index=os.getenv("NEO4J_INDEX"), # The name of the Vector Index in Neo4j
+      node_label=os.getenv("NEO4J_NODE_LABEL"), # Providing a label to Neo4j nodes which store Documents
+      embedding_field="embedding",
+      embedding_dim=768,
+    )
+
+  def markdown_to_vector_embeeding(self, path_to_markdown: Path) -> bool:
+    try:
+      document_converter = MarkdownToDocument(
+        table_to_single_line=False,
+        progress_bar=True,
+        store_full_path=False)
+      document_joiner = DocumentJoiner()
+      document_cleaner = DocumentCleaner(
+        remove_empty_lines=True,
+        remove_extra_whitespaces=True,
+        remove_repeated_substrings=False,
+        keep_id=False,
+        remove_substrings=None,
+        remove_regex=None,
+        unicode_normalization=None,
+        ascii_only=False)
+      document_splitter = DocumentSplitter(
+        split_by="sentence", 
+        split_length=200,
+        split_overlap=50,
+        split_threshold=100,
+        splitting_function=None,
+        respect_sentence_boundary=True,
+        language="en",
+        use_split_rules=True,
+        extend_abbreviations=True,
+        skip_empty_documents=True)
+      document_embedder = OllamaDocumentEmbedder(
+        model=os.getenv("OLLAMA_MODEL"),
+        url=os.getenv("OLLAMA_BASE_URL"))
+      document_writer = DocumentWriter(
+        document_store=self.document_store,
+        policy=DuplicatePolicy.SKIP)
+    
+      self.embedding_pipeline = Pipeline()
+      # Add components to the pipeline
+      self.embedding_pipeline.add_component(instance=document_converter, name="document_converter")
+      self.embedding_pipeline.add_component(instance=document_joiner, name="document_joiner")
+      self.embedding_pipeline.add_component(instance=document_cleaner, name="document_cleaner")
+      self.embedding_pipeline.add_component(instance=document_splitter, name="document_splitter")
+      self.embedding_pipeline.add_component(instance=document_embedder, name="document_embedder")
+      self.embedding_pipeline.add_component(instance=document_writer, name="document_writer")
+      # Connect components
+      self.embedding_pipeline.connect("document_converter", "document_joiner")
+      self.embedding_pipeline.connect("document_joiner", "document_cleaner")
+      self.embedding_pipeline.connect("document_cleaner", "document_splitter")
+      self.embedding_pipeline.connect("document_splitter", "document_embedder")
+      self.embedding_pipeline.connect("document_embedder", "document_writer")
+
+      self.embedding_pipeline.run(
+        data={
+          "file_type_router": {
+            "sources": path_to_markdown
+          }
+        }
+      )
+      return True
+    except Exception as e:
+      print(f"Error in markdown_to_vector_embeeding: {e}")
+      return None
+
+  def query_pipeline_setup(self):
+    try:
+      template = dedent("""
+        Answer the questions based on the given context.
+
+        Context:
+        {% for document in documents %}
+            {{ document.content }}
+        {% endfor %}
+
+        Question: {{ question }}
+        Answer:
+      """)
+
+      embedder = OllamaTextEmbedder(
+        model=os.getenv("OLLAMA_MODEL"),
+        url=os.getenv("OLLAMA_BASE_URL"))
+      retriever = Neo4jEmbeddingRetriever(
+        document_store=self.document_store)
+      augmenter = PromptBuilder(
+        template=template,
+        required_variables=["documents", "question"])
+      generator = OllamaGenerator(
+        model=os.getenv("OLLAMA_MODEL"),
+        url=os.getenv("OLLAMA_BASE_URL"))
+
+      self.query_pipeline = Pipeline()
+      # Add components to the pipeline
+      self.query_pipeline.add_component(instance=embedder, name="embedder")
+      self.query_pipeline.add_component(instance=retriever, name="retriever")
+      self.query_pipeline.add_component(instance=augmenter, name="augmenter")
+      self.query_pipeline.add_component(instance=generator, name="generator")
+      # Connect components
+      self.query_pipeline.connect("embedder.embedding", "retriever.query_embedding")
+      self.query_pipeline.connect("retriever", "augmenter.documents")
+      self.query_pipeline.connect("augmenter.prompt", "generator.messages")
+
+      self.query_pipeline.warm_up()
+
+      return True
+    except Exception as e:
+      print(f"Error in query_pipeline_setup: {e}")
+      return None
+    
+
+  def query(self, question: str) -> str:
+    try:
+      response = self.query_pipeline.run(
+        data={
+          "question": question
+        }
+      )
+      return response["generator"]["replies"][0]
+    except Exception as e:
+      print(f"Error in query: {e}")
+      return None
+
+if __name__ == "__main__":
+  rag_service = RAGService()
+  rag_service.markdown_to_vector_embeeding(
+    path_to_markdown=Path("data/processed_documents/DRG_2E_Rulebook_docling.md")
+  )
+  rag_service.query_pipeline_setup()
+  result = rag_service.query(
+    question="What are soem special features of the driller?"
+  )
+  print(result)
--- backend/app/utils/PDFParser.py
+++ backend/app/utils/PDFParser.py
@@ -2,12 +2,16 @@ import os
 import time
 from io import StringIO
 from pathlib import Path
+from typing import Callable
 
 from docling.datamodel import vlm_model_specs
 from docling.datamodel.accelerator_options import (AcceleratorDevice,
                                                    AcceleratorOptions)
 from docling.datamodel.base_models import InputFormat
 from docling.datamodel.pipeline_options import (PdfPipelineOptions,
+                                                RapidOcrOptions,
+                                                TableFormerMode,
+                                                TesseractOcrOptions,
                                                 VlmPipelineOptions)
 from docling.datamodel.pipeline_options_vlm_model import (ApiVlmOptions,
                                                           ResponseFormat)
@@ -35,6 +39,7 @@ class PDFParser:
       self.pdf_path = pdf_path
 
   def pypdf2_parser(self):
+    """PyPDF2 Parser, produces txt, worst outcome"""
     reader = PyPDF2PdfReader(self.pdf_path)
     output_dir.mkdir(parents=True, exist_ok=True)
     output_path = output_dir / f"{os.path.basename(self.pdf_path).replace('.pdf', '')}_pypdf2.txt"
@@ -44,6 +49,7 @@ class PDFParser:
     print(f"Extracted text saved to: {output_path}")
 
   def pypdf_parser(self):
+    """PyPDF Parser, produces txt, better than pypdf2."""
     reader = PyPDFPdfReader(self.pdf_path)
     output_dir.mkdir(parents=True, exist_ok=True)
     output_path = output_dir / f"{os.path.basename(self.pdf_path).replace('.pdf', '')}_pypdf.txt"
@@ -53,19 +59,32 @@ class PDFParser:
     print(f"Extracted text saved to: {output_path}")
 
   def pdf_miner_parser(self):
-    output_string = StringIO()
-
+    """PDFMiner Parser, produces txt, better than pypdf"""
     with open(self.pdf_path, 'rb') as fin:
       text = extract_text(fin)
-
     output_path = output_dir / f"{os.path.basename(self.pdf_path).replace('.pdf', '')}_pdfminer.txt"
     with open(output_path, "w", encoding="utf-8") as f:
       f.write(text)
     print(f"Extracted text saved to: {output_path}")
 
   def docling_parser(self):
-    converter = DocumentConverter()
-    result = converter.convert(self.pdf_path).document
+    """Default Docling Parser, sucks, don't use it."""
+    pipeline_options = PdfPipelineOptions(
+      generate_table_images=True,
+      enable_parallel_processing=True,
+      do_ocr=True,
+      ocr_options = RapidOcrOptions(lang=["english"]),
+      do_table_structure=True,
+      generate_picture_images=True,
+      generate_page_images=False,
+      do_formula_enrichment=True,
+      table_structure_options={"do_cell_matching": True, "mode": TableFormerMode.ACCURATE},
+      accelerator_options=AcceleratorOptions(num_threads=4, device=AcceleratorDevice.CUDA),
+    )
+
+    format_options = {InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)}
+    converter      = DocumentConverter(format_options=format_options)
+    result         = converter.convert(source=self.pdf_path)
 
     output_path = output_dir / f"{os.path.basename(self.pdf_path).replace('.pdf', '')}_docling.md"
     with open(output_path, "w", encoding="utf-8") as f: 
@@ -73,113 +92,58 @@ class PDFParser:
 
     print(f"Extracted text saved to: {output_path}")
 
-
-
-
-
-  def docling_vlm_parser(self):
-    pipeline_options = VlmPipelineOptions()
-
-    pipeline_options.accelerator_options = AcceleratorOptions(num_threads=8, device=AcceleratorDevice.CUDA)
-    pipeline_options.do_ocr = True
-    pipeline_options.do_table_structure = True
-    pipeline_options.table_structure_options.do_cell_matching = True
-    
-    converter = DocumentConverter(
-      format_options={
-        InputFormat.PDF: PdfFormatOption(
-          pipeline_options=pipeline_options,
-          pipeline_cls=VlmPipeline,
-        )
-      }
+  def docling_ocr_parser(self):
+    """
+    Docling Parser with OCR, uses EasyOCR.
+    uses models provided by docling.
+    produces markdown, with images embedded.
+    Best outcome.
+    """
+    pipeline_options = PdfPipelineOptions(
+      generate_table_images=True,
+      enable_parallel_processing=True,
+      do_ocr=True,
+      ocr_options = RapidOcrOptions(lang=["english"]),
+      do_table_structure=True,
+      generate_picture_images=True,
+      generate_page_images=True,
+      do_formula_enrichment=True,
+      images_scale=2,
+      table_structure_options={"do_cell_matching": True, "mode": TableFormerMode.ACCURATE},
+      accelerator_options=AcceleratorOptions(num_threads=4, device=AcceleratorDevice.CUDA),
     )
-    result = converter.convert(source=self.pdf_path).document
 
-    output_path = output_dir / f"{os.path.basename(self.pdf_path).replace('.pdf', '')}_docling_vlm.md"
+    format_options = {InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)}
+    converter      = DocumentConverter(format_options=format_options)
+    result         = converter.convert(source=self.pdf_path)
+    markdown_text  = result.document.export_to_markdown(image_mode="embedded")
+
+    output_path = output_dir / f"{os.path.basename(self.pdf_path).replace('.pdf', '')}_docling_ocr_base.md"
     with open(output_path, "w", encoding="utf-8") as f:
-      f.write(result.export_to_markdown())
+      f.write(markdown_text)
 
     print(f"Extracted text saved to: {output_path}")
-
-
-
-
-
-  def ollama_vlm_parser(self):
-    # Configure the VLM pipeline. Enabling remote services allows HTTP calls to
-    # locally hosted APIs (LM Studio, Ollama) or cloud services.
-    pipeline_options = VlmPipelineOptions(enable_remote_services=True)
-
-    pipeline_options.accelerator_options = AcceleratorOptions(num_threads=8, device=AcceleratorDevice.CUDA)
-    pipeline_options.do_ocr = True
-    pipeline_options.do_table_structure = True
-    pipeline_options.table_structure_options.do_cell_matching = True
-
-    # Example using the Granite Vision model with Ollama:
-    pipeline_options.vlm_options = ApiVlmOptions(
-      url=f"{ollama_url}/v1/chat/completions",  # the default Ollama endpoint
-      params=dict(
-          model=ollama_model,
-      ),
-      prompt=f"OCR the full page to markdown.",
-      timeout=90,
-      scale=1.0,
-      response_format=ResponseFormat.MARKDOWN,
-    )  
-
-    # Create the DocumentConverter and launch the conversion.
-    doc_converter = DocumentConverter(
-      format_options={
-        InputFormat.PDF: PdfFormatOption(
-          pipeline_options=self._ollama_vlm_pipeline(),
-          pipeline_cls=VlmPipeline,
-        )
-      }
-    )
-    
-    result = doc_converter.convert(source=self.pdf_path).document
-    
-    output_path = output_dir / f"{os.path.basename(self.pdf_path).replace('.pdf', '')}_ollama_vlm.md"
-    with open(output_path, "w", encoding="utf-8") as f:
-      f.write(result.export_to_markdown())
-
-    print(f"Extracted text saved to: {output_path}")
-
-
-
 
 
 
 
 if __name__ == "__main__":
+  """Run all parsers and measure time taken."""
   pdf_parser = PDFParser(DRG_PDF_PATH)
 
-  time_start = time.time()
-  pdf_parser.pypdf2_parser()
-  time_end = time.time()
-  print(f"Time taken pypdf2_parser:      {time_end - time_start} seconds")
-  
-  time_start = time.time()
-  pdf_parser.pypdf_parser()
-  time_end = time.time()
-  print(f"Time taken pypdf_parser:       {time_end - time_start} seconds")
+  func_dict: dict[str, Callable] = {
+    "pypdf2_parser": pdf_parser.pypdf2_parser,
+    "pypdf_parser": pdf_parser.pypdf_parser,
+    "pdf_miner_parser": pdf_parser.pdf_miner_parser,
+    "docling_parser": pdf_parser.docling_parser,
+    "docling_ocr_parser": pdf_parser.docling_ocr_parser,
+  }
 
-  time_start = time.time()
-  pdf_parser.pdf_miner_parser()
-  time_end = time.time()
-  print(f"Time taken pdf_miner_parser:   {time_end - time_start} seconds")
-  
-  # time_start = time.time()
-  # pdf_parser.docling_parser()
-  # time_end = time.time()
-  # print(f"Time taken docling_parser:     {time_end - time_start} seconds")
-  
-  time_start = time.time()
-  pdf_parser.docling_vlm_parser()
-  time_end = time.time()
-  print(f"Time taken docling_vlm_parser: {time_end - time_start} seconds")
-  
-  time_start = time.time()
-  pdf_parser.ollama_vlm_parser()
-  time_end = time.time()
-  print(f"Time taken ollama_vlm_parser:  {time_end - time_start} seconds")
+  for func_name, func in func_dict.items():
+    try:
+      time_start = time.time()
+      func()
+      time_end = time.time()
+      print(f"Time taken {func_name}: {time_end - time_start} seconds")
+    except Exception as e:
+      print(f"Error in {func_name}: {e}")
